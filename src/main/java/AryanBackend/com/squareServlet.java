c0c2b3748bb7fd8fc477a54bd467ad9f00b843d3
Aryan <Aryankamboj2005@gmail.com>
2026-03-12 23:16:49 +0530

    Initial commit

---
+++ src/main/java/AryanBackend/com/squareServlet.java
@@ -0,0 +1,22 @@
+package AryanBackend.com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class squareServlet extends HttpServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException{
+		PrintWriter out = res.getWriter();
+		out.println("this is from the square servlet");
+		// # these code are not valid when we are using the res.sendRedirect("square")
+//		 int k = (int) req.getAttribute("k"); // we are fatching a value of the intType of the name "k" into the 
+//		                            // variable k 
+
+		    PrintWriter outt = res.getWriter();
+		    outt.println("Value from AddServlet: ");
+}}
