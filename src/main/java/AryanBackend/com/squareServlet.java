a7b65938c18ffe4d1908ddd5af9b167c58572419
Aryan <Aryankamboj2005@gmail.com>
2026-03-14 13:01:48 +0530

    sending html and css tage with the servelt so that code works correctly

--- src/main/java/AryanBackend/com/squareServlet.java
+++ src/main/java/AryanBackend/com/squareServlet.java
@@ -34,7 +34,33 @@ public class squareServlet extends HttpServlet {
 			if(c.getName().equals("k"))
 				k=Integer.parseInt(c.getValue());
 		}
-		k = k*k;
-		PrintWriter outt = res.getWriter();
-        outt.println("Value from AddServlet: "+k); //
+//		res.setContentType("text/html");
+//		PrintWriter out1 = res.getWriter();
+//		out1.println("<html><body style='background-color: grey;'>"); // getting css in the servlet 
+//		PrintWriter wr = res.getWriter();
+//		wr.println("after adding the value we get " + k);
+//		k = k*k;
+//		PrintWriter outt = res.getWriter();
+//        outt.println("Value from AddServlet: "+k);
+//        out1.println("</body> </html>"); // closing the tags
+		
+		
+		//      senting html css tags with the servlet to use in the webpage
+		res.setContentType("text/html"); // Tell the browser that the response content is HTML
+
+		PrintWriter out1 = res.getWriter(); // Create a PrintWriter object to send text (HTML) to the browser
+
+		out1.println("<html>");
+		out1.println("<body style='background-color: grey;'>");  // getting css in the servlet
+
+		out1.println("<h2>This is from the square servlet</h2><br>");
+		out1.println("<p>After adding the value we get: " + k + "</p><br>");
+
+		k = k * k;
+
+		out1.println("<p>Value from AddServlet: " + k + "</p><br>");
+
+		out1.println("</body>");
+		out1.println("</html>");
+        
 }}
