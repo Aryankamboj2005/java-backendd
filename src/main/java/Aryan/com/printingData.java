b7510723124984ce82dbffaf87311827b246d845
Aryan <Aryankamboj2005@gmail.com>
2026-03-17 11:27:10 +0530

    we ahve sucessfully get the daat from the java file and print using the jsp file

--- src/main/java/Aryan/com/printingData.java
+++ src/main/java/Aryan/com/printingData.java
@@ -1,5 +1,25 @@
 package Aryan.com;
 
-public class printingData {
+import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/printingData")
+public class printingData extends HttpServlet {
+
+    public void service(HttpServletRequest req, HttpServletResponse res)
+            throws IOException, ServletException {
+
+        String name = "Aryan";
+
+        req.setAttribute("name", name);
+
+        RequestDispatcher rd = req.getRequestDispatcher("basic.jsp");
+        rd.forward(req, res);
+    }
 }
