e896560b29b0f04cfba218f02f713e6ae7d8f510
Aryan <Aryankamboj2005@gmail.com>
2026-03-22 08:38:47 +0530

    removing the error from the lasrt commit data

--- src/main/java/learning/filter.java
+++ src/main/java/learning/filter.java
@@ -1,12 +1,11 @@
 package learning;
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.*;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/filter")
 public class filter  extends HttpServlet{
 	public void service(HttpServletRequest req, HttpServletResponse res) 
